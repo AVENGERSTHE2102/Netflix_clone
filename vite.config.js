57f4ae47c70bf3149bfa6ab69ab05fe682c234a2
AVENGERSTHE2102 <sabnisa7@gmail.com>
2026-04-13 18:44:13 +0530

    build changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.js$/,
